546c69287e9ce5a595db73e9e5f323ef1392e4ba
jakegodsall <jake.edward.godsall@gmail.com>
2023-09-11 11:45:08 +0200

    initial commit.

---
+++ src/main/java/com/jakegodsall/audiophilerestapi/AudiophileRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.audiophilerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiophileRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiophileRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakegodsall/audiophilerestapi/AudiophileRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.audiophilerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiophileRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
